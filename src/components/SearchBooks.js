646d750a2208ce4dacebea81e247f703af50b2f8
Klaudia Nemeth <knemeth@vidatec.com>
2020-08-06 13:10:41 +0200

    start using  React Hooks

--- src/components/SearchBooks.js
+++ src/components/SearchBooks.js
@@ -1,80 +1,58 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Select, MenuItem } from "@material-ui/core";
 import BookList from "./BookList";
 import "../styles/SearchBooks.css";
 
-class SearchBooks extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { categories: [], books: [], category: "" };
-    this.handleCategoryChange = this.handleCategoryChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+function SearchBooks() {
+  const [menu, setMenu] = useState([]);
+  const [books, setBooks] = useState([]);
+  const [category, setCategory] = useState('');
+  const [url, setUrl] = useState("https://api.nytimes.com/svc/books/v3/lists/names.json?api-key=OGDK7aGVDlAT6L8KaYnfASlYi6ydHveG");
 
-  async handleSubmit(event) {
-    event.preventDefault();
-    if (this.state.category === "") {
-      return;
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+        const data = await response.json();
+        data.results.books ? setBooks(data.results.books) : setMenu(data.results);
+      } catch (error) {
+        console.error(error);
+      }
     }
-    try {
-      const url = `https://api.nytimes.com/svc/books/v3/lists/current/${this.state.category}.json?api-key=OGDK7aGVDlAT6L8KaYnfASlYi6ydHveG`;
-      const response = await fetch(url);
-      if (!response.ok) {
-        throw Error(response.statusText);
-      }
-      const data = await response.json();
+    fetchData();
+  }, [url]);
 
-      this.setState(() => {
-        return {
-          ...this.state,
-          books: data.results.books,
-        };
-      });
-    } catch (error) { console.error(error)}
+
+  const handleOnSubmit = event => {
+    event.preventDefault();
+    category !== '' ? setUrl(`https://api.nytimes.com/svc/books/v3/lists/current/${category}.json?api-key=OGDK7aGVDlAT6L8KaYnfASlYi6ydHveG`) : alert("Category cannot be empty!!!");
   }
 
-  handleCategoryChange(e) {
-    this.setState({ category: e.target.value });
-  }
-  async componentDidMount() {
-    try {
-      const url =
-        "https://api.nytimes.com/svc/books/v3/lists/names.json?api-key=OGDK7aGVDlAT6L8KaYnfASlYi6ydHveG";
-      const response = await fetch(url);
-      if (!response.ok) {
-        throw Error(response.statusText);
-      }
-      const data = await response.json();
-      this.setState({ categories: data.results });
-    } catch (error) { console.error(error)}
-  }
-
-  render() {
-    return (
-      <React.Fragment>
-        <form className="form" onSubmit={this.handleSubmit}>
-          <Select
-            className="select"
-            labelId="category"
-            id="category"
-            value={this.state.category}
-            onChange={this.handleCategoryChange}
-          >
-            {this.state.categories.map(
-              ({ list_name_encoded, display_name }) => (
-                <MenuItem value={list_name_encoded} key={list_name_encoded}>
-                  {display_name}
-                </MenuItem>
-              )
-            )}
-          </Select>
-          <button className="button" type="submit">
-            Search
-          </button>
-        </form>
-        <BookList books={this.state.books} />
-      </React.Fragment>
-    );
-  }
+  return (
+    <React.Fragment>
+      <form className="form" onSubmit={handleOnSubmit} >
+        <Select
+          className="select"
+          labelId="category"
+          id="category"
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
+        >
+          {menu.map(({ list_name_encoded, display_name }) => (
+            <MenuItem value={list_name_encoded} key={list_name_encoded}>
+              {display_name}
+            </MenuItem>
+          ))}
+        </Select>
+        <button className="button" type="submit">
+          Search
+        </button>
+      </form>
+      <BookList books={books} />
+    </React.Fragment>
+  );
 }
 export default SearchBooks;
